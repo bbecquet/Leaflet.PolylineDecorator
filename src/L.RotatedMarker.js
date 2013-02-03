164705dd1c0f3ef4e94f181d5bc26f176f4883ba
Benjamin Becquet <benjamin.becquet+github@gmail>
2013-02-04 00:03:23 +0100

    More robust usage of CSS transform properties for rotated markers. Fixes #7

--- src/L.RotatedMarker.js
+++ src/L.RotatedMarker.js
@@ -2,20 +2,30 @@ L.RotatedMarker = L.Marker.extend({
     options: {
         angle: 0
     },
+	statics: {
+		// determine the best and only CSS transform rule to use for this browser
+		bestTransform: L.DomUtil.testProp([
+			'transform',
+			'WebkitTransform',
+			'msTransform',
+			'MozTransform',
+			'OTransform'
+		])
+	},
     _setPos: function (pos) {
         L.Marker.prototype._setPos.call(this, pos);
-        if (!L.Browser.ie || L.Browser.ie3d) {
-            var rotate = ' rotate(' + this.options.angle + 'deg)';
-            this._icon.style.MozTransform += rotate;
-            this._icon.style.MsTransform += rotate;
-            this._icon.style.OTransform += rotate;
-            this._icon.style.WebkitTransform += rotate;
-        } else {
-            // IE6, IE7, IE8
-            var rad = this.options.angle * L.LatLng.DEG_TO_RAD,
-                costheta = Math.cos(rad),
-                sintheta = Math.sin(rad);
-            this._icon.style.filter += ' progid:DXImageTransform.Microsoft.Matrix(sizingMethod=\'auto expand\', M11=' + costheta + ', M12=' + (-sintheta) + ', M21=' + sintheta + ', M22=' + costheta + ')';
-        }
+		
+		var rotate = ' rotate(' + this.options.angle + 'deg)';
+		if (L.RotatedMarker.bestTransform) {
+			// use the CSS transform rule if available
+			this._icon.style[L.RotatedMarker.bestTransform] += rotate;
+		} else if(L.Browser.ie) {
+			// fallback for IE6, IE7, IE8
+			var rad = this.options.angle * L.LatLng.DEG_TO_RAD,
+				costheta = Math.cos(rad),
+				sintheta = Math.sin(rad);
+			this._icon.style.filter += ' progid:DXImageTransform.Microsoft.Matrix(sizingMethod=\'auto expand\', M11=' + 
+				costheta + ', M12=' + (-sintheta) + ', M21=' + sintheta + ', M22=' + costheta + ')';				
+		}
     }
 });
