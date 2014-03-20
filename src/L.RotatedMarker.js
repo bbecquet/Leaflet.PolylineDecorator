1fdfbf599f7129b0354b4b778ce40ba4785d56ab
Benjamin Becquet <benjamin.becquet@gmail.com>
2014-03-21 00:12:10 +0100

    Leaflet 0.8-dev: remove use of L.LatLng.DEG_TO_RAD

--- src/L.RotatedMarker.js
+++ src/L.RotatedMarker.js
@@ -2,6 +2,7 @@ L.RotatedMarker = L.Marker.extend({
     options: {
         angle: 0
     },
+
     _setPos: function (pos) {
         L.Marker.prototype._setPos.call(this, pos);
         
@@ -10,7 +11,7 @@ L.RotatedMarker = L.Marker.extend({
             this._icon.style[L.DomUtil.TRANSFORM] += ' rotate(' + this.options.angle + 'deg)';
         } else if(L.Browser.ie) {
             // fallback for IE6, IE7, IE8
-            var rad = this.options.angle * L.LatLng.DEG_TO_RAD,
+            var rad = this.options.angle * (Math.PI / 180),
                 costheta = Math.cos(rad),
                 sintheta = Math.sin(rad);
             this._icon.style.filter += ' progid:DXImageTransform.Microsoft.Matrix(sizingMethod=\'auto expand\', M11=' + 
