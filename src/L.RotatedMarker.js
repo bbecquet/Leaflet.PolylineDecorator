812d0ac272286891e591a7b6b0c09000341b3672
Benjamin Becquet <benjamin.becquet@gmail.com>
2015-09-29 16:58:56 +0200

    Fix shifted rotated marker. Fix #38

--- src/L.RotatedMarker.js
+++ src/L.RotatedMarker.js
@@ -3,9 +3,20 @@ L.RotatedMarker = L.Marker.extend({
         angle: 0
     },
 
+    statics: {
+        TRANSFORM_ORIGIN: L.DomUtil.testProp(
+            ['transformOrigin', 'WebkitTransformOrigin', 'OTransformOrigin', 'MozTransformOrigin', 'msTransformOrigin'])
+    },
+
+    _initIcon: function() {
+        L.Marker.prototype._initIcon.call(this);
+
+        this._icon.style[L.RotatedMarker.TRANSFORM_ORIGIN] = '50% 50%';
+    },
+
     _setPos: function (pos) {
         L.Marker.prototype._setPos.call(this, pos);
-        
+
         if (L.DomUtil.TRANSFORM) {
             // use the CSS transform rule if available
             this._icon.style[L.DomUtil.TRANSFORM] += ' rotate(' + this.options.angle + 'deg)';
@@ -14,10 +25,11 @@ L.RotatedMarker = L.Marker.extend({
             var rad = this.options.angle * (Math.PI / 180),
                 costheta = Math.cos(rad),
                 sintheta = Math.sin(rad);
-            this._icon.style.filter += ' progid:DXImageTransform.Microsoft.Matrix(sizingMethod=\'auto expand\', M11=' + 
-                costheta + ', M12=' + (-sintheta) + ', M21=' + sintheta + ', M22=' + costheta + ')';                
+            this._icon.style.filter += ' progid:DXImageTransform.Microsoft.Matrix(sizingMethod=\'auto expand\', M11=' +
+                costheta + ', M12=' + (-sintheta) + ', M21=' + sintheta + ', M22=' + costheta + ')';
         }
     },
+
     setAngle: function (ang) {
         this.options.angle = ang;
     }
